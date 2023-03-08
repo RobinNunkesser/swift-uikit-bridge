e084825db77a18b06b114bd72b32781a197c8a58
Robin Nunkesser <robin.nunkesser@hshl.de>
2023-03-08 15:54:43 +0100

    Bridge start

--- BridgeExample/ViewController.swift
+++ BridgeExample/ViewController.swift
@@ -8,21 +8,24 @@
 import UIKit
 import WebKit
 
+let doStuffMessageHandler = "doStuffMessageHandler"
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var webViewContainer: UIView!
-    let webView: WKWebView
+    var webView: WKWebView
     
     required init?(coder: NSCoder) {
-        let configuration = WKWebViewConfiguration()
-        webView = WKWebView(frame: .zero, configuration: configuration)
-
         super.init(coder: coder)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let configuration = WKWebViewConfiguration()
+        configuration.userContentController.add(self, name: doStuffMessageHandler)
+        webView = WKWebView(frame: .zero,configuration: configuration)
+
         webView.translatesAutoresizingMaskIntoConstraints = false
         webViewContainer.addSubview(webView)
         
@@ -31,9 +34,26 @@ class ViewController: UIViewController {
             webView.heightAnchor.constraint(equalTo: webViewContainer.heightAnchor)
         ])
         
-        webView.load(URLRequest(url: URL(string:"https://www.apple.com")!))
+        if let url = Bundle.main.url(forResource: "index", withExtension: "html") {
+            webView.load(URLRequest(url: url))
+        }
     }
 
 
+}
+
+extension ViewController : WKScriptMessageHandler {
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        if message.name == doStuffMessageHandler {
+            guard let dict = message.body as? [String: AnyObject],
+                  let param1 = dict["param1"] as? String,
+                  let param2 = dict["param2"] as? Int
+            else {
+                return
+            }
+            debugPrint(param1)
+            debugPrint(param2)
+        }
+    }
 }
 

19b5f7287a7fa3804a3b269f76bdf499b7ca7ca3
Robin Nunkesser <robin.nunkesser@hshl.de>
2023-03-08 14:10:12 +0100

    Integrate WKWebView

--- BridgeExample/ViewController.swift
+++ BridgeExample/ViewController.swift
@@ -6,12 +6,32 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webViewContainer: UIView!
+    let webView: WKWebView
+    
+    required init?(coder: NSCoder) {
+        let configuration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: configuration)
+
+        super.init(coder: coder)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        webViewContainer.addSubview(webView)
+        
+        NSLayoutConstraint.activate([
+            webView.widthAnchor.constraint(equalTo: webViewContainer.widthAnchor),
+            webView.heightAnchor.constraint(equalTo: webViewContainer.heightAnchor)
+        ])
+        
+        webView.load(URLRequest(url: URL(string:"https://www.apple.com")!))
     }
 
 
